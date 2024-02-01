b49acbf2cac0accdaf42d038cf51a3e1ddfdd819
Caio Henrique <104094613+caiohenrique-3@users.noreply.github.com>
2024-02-01 20:04:25 -0300

    chore: Remove unused endpoint

--- src/main/java/controller/FileUploadController.java
+++ src/main/java/controller/FileUploadController.java
@@ -12,8 +12,6 @@ import org.springframework.web.multipart.MultipartFile;
 import utility.FileUploadUtil;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 // Code for upload & download
 // https://www.codejava.net/frameworks/spring-boot/file-download-upload-rest-api-examples
@@ -39,35 +37,10 @@ public class FileUploadController {
                 .body(response);
     }
 
-    @PostMapping("/single-file-upload")
-    public ResponseEntity<Map<String, String>> handleFileUploadUsingCurl(
-            @RequestParam("file") MultipartFile file) throws IOException {
-        Map<String, String> map = new HashMap<>();
-
-        populateMapWithFileDetails(map, file);
-
-        return ResponseEntity.ok(map);
-    }
-
-    public void populateMapWithFileDetails(Map<String, String> mp, MultipartFile file) {
-        mp.put("fileName", file.getOriginalFilename());
-
-        double size = getRoundedFileSizeInMegabytes(file);
-        mp.put("fileSize", String.valueOf(size));
-
-        mp.put("fileContentType", file.getContentType());
-    }
-
     public double getRoundedFileSizeInMegabytes(MultipartFile file) {
         long fileSizeInBytes = file.getSize();
         double fileSizeInMegabytes = fileSizeInBytes / (1024.0 * 1024.0);
         fileSizeInMegabytes = Math.round(fileSizeInMegabytes * 100.0) / 100.0;
         return fileSizeInMegabytes;
-    }
-
-    @GetMapping("/download-file/{fileCode}")
-    public ResponseEntity<String> getDownload() {
-        return ResponseEntity
-                .ok("nothing!");
     }
 }
