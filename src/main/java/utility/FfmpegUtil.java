930ba6f9a71a814bbf2e8b2c64ee7378727f9e18
Caio Henrique <104094613+caiohenrique-3@users.noreply.github.com>
2024-02-08 19:16:21 -0300

    refactor: Remove duplicate code & extract method

--- src/main/java/utility/FfmpegUtil.java
+++ src/main/java/utility/FfmpegUtil.java
@@ -8,47 +8,43 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Logger;
 
 public class FfmpegUtil {
-    static Logger logger =
-            Logger.getLogger(FfmpegUtil.class.getName());
-
     public static void processFile(AudioFile file, String slowed) {
-        if (slowed != null && slowed.equals("true"))
-            slowDown(file);
-        else
-            nightcore(file);
+        double asetRateModifier = 0;
+        double atempo = 0;
 
-        String fileCode = file.getFileCode();
-        file.setFileCode("/download/" + fileCode);
+        if (slowed != null && slowed.equals("true")) {
+            asetRateModifier = 0.9;
+            atempo = 1.0;
+        } else {
+            asetRateModifier = 1.25;
+            atempo = 1.06;
+        }
+
+        modifyAudioFile(file, asetRateModifier, atempo);
+
+        file.setFileCode("/download/" + file.getFileCode());
     }
 
-    private static void slowDown(AudioFile file) {
+    private static void modifyAudioFile(AudioFile file,
+                                        double asetRateModifier,
+                                        double atempo) {
         String songPath = Paths.get("user-uploaded-files", file.getFileCode() +
                 "-" + file.getFileName()).toAbsolutePath().toString();
 
-        double asetRateModifier = 0.9;
-
-        executeRuntimeCommand(songPath, asetRateModifier);
+        executeRuntimeCommand(songPath, asetRateModifier, atempo);
     }
 
-    private static void nightcore(AudioFile file) {
-        String songPath = Paths.get("user-uploaded-files", file.getFileCode() +
-                "-" + file.getFileName()).toAbsolutePath().toString();
-
-        double asetRateModifier = 1.25;
-
-        executeRuntimeCommand(songPath, asetRateModifier);
-    }
-
-    private static void executeRuntimeCommand(String songPath, double asetRateModifier) {
+    private static void executeRuntimeCommand(
+            String songPath, double asetRateModifier,
+            double atempo) {
         try {
             Path songParentDirectory =
                     Paths.get(songPath).getParent().toAbsolutePath();
 
             List<String> commands =
-                    createFfmpegCommandBody(songPath, asetRateModifier);
+                    createFfmpegCommandBody(songPath, asetRateModifier, atempo);
 
             setupAndStartFfmpegProcess(songParentDirectory, commands);
 
@@ -58,7 +54,8 @@ public class FfmpegUtil {
     }
 
     private static List<String> createFfmpegCommandBody(
-            String songPath, double asetRateModifier) {
+            String songPath,
+            double asetRateModifier, double atempo) {
         List<String> commandBody = new ArrayList<>();
 
         commandBody.add("ffmpeg");
@@ -66,14 +63,16 @@ public class FfmpegUtil {
         commandBody.add("-i");
         commandBody.add(songPath);
         commandBody.add("-filter:a");
-        commandBody.add("atempo=1.0,asetrate=44100*" + asetRateModifier);
+        commandBody.add("atempo=" + atempo + ",asetrate=44100*" + asetRateModifier);
         commandBody.add("-vn");
         commandBody.add("-map");
         commandBody.add("0");
         commandBody.add("-map_metadata");
         commandBody.add("0:g");
+
         File f = new File(songPath);
         String tempFilePath = f.getParent() + "/temp_" + f.getName();
+
         commandBody.add(tempFilePath);
 
         return commandBody;
@@ -83,27 +82,29 @@ public class FfmpegUtil {
             Path workingDirectory, List<String> commands)
             throws IOException, InterruptedException {
         ProcessBuilder processBuilder = new ProcessBuilder(commands);
-
         processBuilder.directory(workingDirectory.toFile());
         processBuilder.redirectErrorStream(true);
-
         Process process = processBuilder.start();
 
         int exitCode = process.waitFor();
 
         if (exitCode == 0) {
-            File originalFile = new File(commands.get(3));
-            File tempFile = new File(commands.get(commands.size() - 1));
-
-            if (originalFile.delete() && tempFile.renameTo(originalFile)) {
-                System.out.println("[FFMPEG]: File replaced successfully");
-            } else {
-                System.out.println("[FFMPEG]: Failed to replace file");
-            }
+            replaceOldFileWithFfmpegOutput(commands);
 
             System.out.println("[FFMPEG]: Success");
         } else {
             System.out.println("[FFMPEG]: Failed with exit code " + exitCode);
+        }
+    }
+
+    private static void replaceOldFileWithFfmpegOutput(List<String> commands) {
+        File originalFile = new File(commands.get(3));
+        File tempFile = new File(commands.get(commands.size() - 1));
+
+        if (originalFile.delete() && tempFile.renameTo(originalFile)) {
+            System.out.println("[FFMPEG]: File replaced successfully");
+        } else {
+            System.out.println("[FFMPEG]: Failed to replace file");
         }
     }
 }
